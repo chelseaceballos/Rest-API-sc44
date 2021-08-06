9f5b222e7f0cc4269552bfd577a88b1b1b8ef746
Chelsea Ceballos <chelsea-ceballos@lambdastudents.com>
2021-08-05 19:15:10 -0700

    25/27 passing, will continue tomorrow

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -41,15 +41,48 @@ router.post('/', (req, res, next) => {
     }
 })
 
-router.put('/:id', (req, res, next) => {
-    // Returns the updated action as the body of the response.
-    // If there is no action with the given id it responds with a status code 404.
-    // If the request body is missing any of the required fields it responds with a status code 400.
+// router.put('/:id', (req, res, next) => { //async??
+//   const { description, notes} = req.body
+//   if(!description || !notes) {
+//     res.status(400).json({ message: "required fields empty" })
+//   } else {
+//       Actions.get(req.params.id)
+//       .then(stuff =>{
+//           if(!stuff) {
+//              res.status(404).json({message : "The project with this ID doesn't exist"})
+//           } else {
+//               return Projects.update(req.params.id, req.body)
+//           }
+//       })
+//       .catch(next)
+//   }
+// })
+
+router.put('/:id', (req, res, next) => { //async??
+  
+    if (!req.body.project_id || !req.body.description || !req.body.notes) {
+        res.status(404).json('required fields left empty')
+    } else {
+        Actions.update(req.params.id, req.body)
+        .then(updated => {
+            res.status(201).json(updated)
+        }) 
+        .catch(next)
+    } 
 })
 
 router.delete('/:id', (req, res, next) => {
     // Returns no response body.
     // If there is no action with the given id it responds with a status code 404.
+    Actions.remove(req.params.id)
+    .then(count =>{
+        if(count > 0) {
+            res.status(200).json({message: `Action id:${req.params.id} deleted`})
+        } else {
+            res.status(404).json('Action does not exist')
+        }
+    })
+    .catch(next)
 })
 
 router.use((err, req, res ,next) =>{
