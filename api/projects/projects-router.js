ddf72957a7fb7d12a26874b572958b3e14ad2471
Chelsea Ceballos <chelsea-ceballos@lambdastudents.com>
2021-08-05 18:33:08 -0700

    project tests passing, all but 1 on put, will do middleware funcs later, I really really need a refresh

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -19,7 +19,7 @@ router.get('/:id', (req, res, next) => {
     Projects.get(req.params.id)
     .then(project => {
         if(!project) {
-            res.status(404).json({message: `Project with ${req.params.id} ID does not exist`})
+            res.status(404).json({message: `Project with ID:${req.params.id} does not exist`})
         } else {
             res.json(project)
         }
@@ -27,10 +27,8 @@ router.get('/:id', (req, res, next) => {
 })
 
 router.post('/',  (req, res, next) => {
-    //     Returns the newly created project as the body of the response.
-    // If the request body is missing any of the required fields it responds with a status code 400.
     if (!req.body.name || !req.body.description){
-        res.status(400).json('missing stuff')
+        res.status(400).json('Project name & description required!')
     } else {
         Projects.insert(req.body)
         .then(project =>{
@@ -38,25 +36,43 @@ router.post('/',  (req, res, next) => {
         })
         .catch(next)
     }
-
 })
 
-router.put('/:id', (req, res, next) => {
+router.put('/:id', (req, res, next) => { // 1/3 FAILING
     // Returns the updated project as the body of the response.
-    // If there is no project with the given id it responds with a status code 404.
+    // (HERE) If there is no project with the given id it responds with a status code 404.
     // If the request body is missing any of the required fields it responds with a status code 400.
+    if(!req.body.name || !req.body.description){
+        res.status(400).json('missing name and description')
+    } else {
+        Projects.update(req.params.id, req.body)
+        .then(project =>{
+            res.status(201).json(project)
+        })
+        .catch(next)
+    }
 })
 
 router.delete('/:id', (req, res, next) => {
-    // Returns no response body.
-    // If there is no project with the given id it responds with a status code 404.
+    Projects.remove(req.params.id)
+    .then(count =>{
+        if(count > 0) {
+            res.status(200).json({message: `Project ${req.params.id} has been deleted`})
+        } else {
+            res.status(404).json('The project does not exist')
+        }
+    }) .catch(next)
 })
 
 router.get('/:id/actions', (req, res, next) => {
-    // Returns an array of actions (could be empty) belonging to a project with the given id.
-    // If there is no project with the given id it responds with a status code 404.
-    // Projects.getProjectActions()
-
+    Projects.getProjectActions(req.params.id)
+    .then(project =>{
+        if(!project) {
+            res.status(404).json({message: `No actions belong to the project with the id of ${req.params.id}`})
+        } else {
+           res.status(200).json(project) 
+        }
+    }) .catch(next)
 })
 
 router.use((err, req, res ,next) =>{
